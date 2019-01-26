61b54bfc06fd236f3475cac271a31e8fbb237bc4
Thomas Lock <email@peb.si>
2019-01-26 22:53:00 +0000

    change references to game to world

--- game.lua
+++ game.lua
@@ -1,74 +1,35 @@
-
-game = {}
-local MAP_WIDTH = 100
-local MAP_HEIGHT = 100
-local TILE_WIDTH = 32
-local TILE_HEIGHT = 32
-
-function game.findTileSize()
-    TILE_WIDTH = 32
-    return TILE_WIDTH
-end
+world = {}
 
 worldImg = {}
 worldImg["Grass"] = love.graphics.newImage("assets/Grass.png")
 worldImg["Forest"] = love.graphics.newImage("assets/Forest.png")
 worldImg["Mine"] = love.graphics.newImage("assets/Mine.png")
 
+function updateWorld()
+    world = {}
 
--- Define tile types here
-
-
-
-
--- End of tile types definition
-
-local rans = {13,14,15}
-function myRandom()
-    return rans[math.random(#rans)]
-end
-
---game.map = {}
-
---for i = 1, 100 do
- --   game.map[i] = {}
- --   for k = 1, 100 do
- --       game.map[i][k] = myRandom()
- --   end
-
-
-function getMap()
-    newGameMap = {}
-
-    --Initialise world with grass
-
-    for i = 1, 100*100 do
-        newGameMap[i] = "Grass"
-    end
     b, c, h = http.request("http://freshplay.co.uk/b/api.php?a=get&scope=world")
-    json = require("json")
 
     world_decoded = json:decode(b)
     for i, v in pairs(world_decoded) do
-        newGameMap[i] = v.buildingType
-        print(v.buildingType)
+        world[i] = v
     end
 end
 
-function game.load()
-    getMap()
+function world.load()
+    updateWorld()
 end
 
-function game.draw()
-
+function world.draw()
     local x = 0
     local y = 0
     for i = 1, 100*100 do
-        if worldImg[newGameMap[i]] then
-            love.graphics.draw(worldImg[newGameMap[i]], x, y)
+        if worldImg[world[i]] then
+            love.graphics.draw(worldImg[world[i]], x, y)
         else
-          print("CAN'T FIND BUILDING TYPE "..tostring(newGameMap[i]))
+            love.graphics.rectangle("fill",x,y,32,32)
         end
+
         x = x + 32
         if x >= 100*32 then
             x = 0
@@ -76,16 +37,16 @@ function game.draw()
         end
     end
 
-    local mCol = math.floor(love.mouse.getX() / TILE_WIDTH) + 1
+   --[[ local mCol = math.floor(love.mouse.getX() / TILE_WIDTH) + 1
     local mLine = math.floor(love.mouse.getY() / TILE_HEIGHT) + 1
     if mCol >= 1 and mCol <= MAP_WIDTH and mLine >= 1 and mLine <= MAP_HEIGHT then 
-        local id = newGameMap[mCol + ((mLine - 1) * 100)]
+        local id = world[mCol + ((mLine - 1) * 100)]
         love.graphics.print("ID : "..tostring(id), 1, 1)
         love.graphics.print("X : "..tostring(mCol).. " Y : "..tostring(mLine), 100, 1)
     else
         love.graphics.print("Not a tile")
-    end
+    end]]
 end
 
 
-return game
+return world
--- main.lua
+++ main.lua
@@ -1,45 +1,30 @@
 http = require("socket.http")
-local myGame = require("game")
+json = require("json")
+local world = require("world")
+
+phase = "world"
 
 
 function love.load()
-
-    -- in the main chunk somewhere:
-
--- every time after resolution changes, and also at initialization: 
-
-
-    myGame.load()
-end
-
-function findTile(x)
-    return x * myGame.findTileSize()
+    world.load()
 end
 
 function love.update(dt)
-    -- To move the cursor/current unit
-    -- with a smooth L33T transition
-	--player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
-	--player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
 end
 
 function love.draw()
-
-    myGame.draw()
+    if phase == "world" then
+        world.draw()
+    end
 end
 
 function love.keypressed(key)
-    
 end
 
 function love.mousepressed(x, y, button, istouch)
     mouseX = x
     mouseY = y
-    -- mouseX = math.floor(mouseX/myGame.findTileSize()) * findTileSize()
-    -- mouseY = math.floor(mouseY/myGamer.findTileSize()) * findTileSize() 
 end
-
--- Make HTTP request to http://freshplay.co.uk/b/api.php?a=get&scope=world&type=buildings
 
 
 
